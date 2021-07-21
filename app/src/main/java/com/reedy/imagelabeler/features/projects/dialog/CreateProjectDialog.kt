33533aff5b34f334a662ef09a7729ef708ae2ad2
John Reedy <jwr09@yahoo.com>
2021-07-21 19:27:51 -0400

    Built the project dialog fragment

---
+++ app/src/main/java/com/reedy/imagelabeler/features/projects/dialog/CreateProjectDialog.kt
@@ -0,0 +1,11 @@
+package com.reedy.imagelabeler.features.projects.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+
+class CreateProjectDialog(context: Context): Dialog(context) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
